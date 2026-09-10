48edcf698fbd25c33c0793cdc7ee1d7bb615076f
lai-long <3496629516@qq.com>
2026-09-10 12:26:40 +0800

    feat(comment)：删除评论

--- blog/internal/logic/admin/commentdeletelogic.go
+++ blog/internal/logic/admin/commentdeletelogic.go
@@ -5,9 +5,12 @@ package admin
 
 import (
 	"context"
+	"errors"
 
+	"myblog_backend/blog/internal/model"
 	"myblog_backend/blog/internal/svc"
 	"myblog_backend/blog/internal/types"
+	"myblog_backend/pkg/errx"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -27,7 +30,18 @@ func NewCommentDeleteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Com
 }
 
 func (l *CommentDeleteLogic) CommentDelete(req *types.CommentIdReq) (resp *types.EmptyResp, err error) {
-	// todo: add your logic here and delete this line
+	// 先确认存在（FindOne 已排除软删的，删过的再次请求就是 404）
+	if _, err := l.svcCtx.CommentModel.FindOne(l.ctx, req.Id); err != nil {
+		if errors.Is(err, model.ErrNotFound) {
+			return nil, errx.New(errx.NotFound, "评论不存在")
+		}
+		return nil, errx.Wrap(err, errx.ServerError, "查询评论失败")
+	}
 
-	return
+	// 软删除：只写 deleted_at
+	if err := l.svcCtx.CommentModel.Delete(l.ctx, req.Id); err != nil {
+		return nil, errx.Wrap(err, errx.ServerError, "删除评论失败")
+	}
+
+	return &types.EmptyResp{}, nil
 }
